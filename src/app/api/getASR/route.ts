be9d3babd5cb6e889dd605799ee00abb7b0d3ed8
Dylan <dylan.reeder5@gmail.com>
2023-08-01 11:39:04 -0400

    segments of audio

--- src/app/api/getASR/route.ts
+++ src/app/api/getASR/route.ts
@@ -1,32 +1,32 @@
 import {HfInference} from '@huggingface/inference';
-import {readFileSync} from 'fs';
+import fs from 'fs';
+import path from 'path';
 
 const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
 export async function POST() {
   // get audio from video
-
-  const arrayBuffer = getAudioBuffer();
-  try {
-    const text = Hf.automaticSpeechRecognition({
-      model: 'jonatasgrosman/wav2vec2-large-xlsr-53-english',
-      data: arrayBuffer,
-    });
-
-    text
-      .then((text) => {
-        console.log('text: -------', text);
-      })
-      .catch((err) => {
-        console.log('error: -------', err);
+  const dir = fs.opendirSync('chunks');
+  let dirent;
+  while ((dirent = dir.readSync()) !== null) {
+    const buffer = fs.readFileSync(path.join('chunks', dirent.name));
+    const arrayBuffer = Uint8Array.from(buffer).buffer;
+    try {
+      const text = Hf.automaticSpeechRecognition({
+        model: 'jonatasgrosman/wav2vec2-large-xlsr-53-english',
+        data: arrayBuffer,
       });
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-function getAudioBuffer() {
-  const buffer = readFileSync('audio.mp3');
-  const arrayBuffer = Uint8Array.from(buffer).buffer;
-  return arrayBuffer;
+      text
+        .then((text) => {
+          console.log('text: -------', text);
+        })
+        .catch((err) => {
+          console.log('error: -------', err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  dir.closeSync();
 }
--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -1,43 +1,49 @@
 import cp from 'child_process';
 import fs from 'fs';
 import ytdl from 'ytdl-core';
+import path from 'path';
 
 export async function POST(req: Request) {
   const {video} = await req.json();
-  await new Promise<void>((resolve, reject) => {
-    ytdl.getInfo(video).then((info) => {
-      const format = ytdl.chooseFormat(info.formats, {quality: 'highestaudio'});
-      const start = Date.now();
+  ytdl.getInfo(video).then((info) => {
+    const format = ytdl.chooseFormat(info.formats, {quality: 'highestaudio'});
+    const start = Date.now();
 
-      const ffmpegProcess = cp.spawn(
-        'ffmpeg',
-        ['-i', 'pipe:3', '-f', 'mp3', 'pipe:4'],
-        {
-          windowsHide: true,
-          stdio: [
-            /* Standard: stdin, stdout, stderr */
-            'inherit',
-            'inherit',
-            'inherit',
-            /* Custom: pipe:3, pipe:4 */
-            'pipe',
-            'pipe',
-          ],
-        }
-      );
+    const ffmpegProcess = cp.spawn(
+      'ffmpeg',
+      [
+        '-i',
+        'pipe:3',
+        '-map',
+        '0:a', // map only audio stream
+        '-f',
+        'segment',
+        '-segment_time',
+        '30', // split every 30 seconds
+        '-c:a',
+        'libmp3lame', // use libmp3lame codec for audio
+        path.join('chunks', 'output%03d.mp3'), // output pattern
+      ],
+      {
+        windowsHide: true,
+        stdio: [
+          /* Standard: stdin, stdout, stderr */
+          'inherit',
+          'inherit',
+          'inherit',
+          /* Custom: pipe:3, pipe:4 */
+          'pipe',
+          'pipe',
+        ],
+      }
+    );
 
-      ffmpegProcess.on('close', () => {
-        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-        resolve();
-      });
-
-      ffmpegProcess.on('error', reject);
-
-      ytdl
-        .downloadFromInfo(info, {format: format})
-        .pipe(ffmpegProcess.stdio[3] as any);
-
-      ffmpegProcess.stdio[4]?.pipe(fs.createWriteStream('audio.mp3'));
+    ffmpegProcess.on('close', () => {
+      console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
     });
+
+    ytdl
+      .downloadFromInfo(info, {format: format})
+      .pipe(ffmpegProcess.stdio[3] as any);
   });
 }
