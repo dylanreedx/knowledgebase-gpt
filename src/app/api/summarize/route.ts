dd2efdf02750ac40fda22dff1da6ad055d0eef70
Dylan <dylan.reeder5@gmail.com>
2023-08-07 11:07:52 -0400

    fix dev api url

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -6,8 +6,8 @@ import {PromptTemplate} from 'langchain/prompts';
 import {RecursiveCharacterTextSplitter} from 'langchain/text_splitter';
 import {NextResponse} from 'next/server';
 
-const URL = 'http://localhost:3001';
-// const URL = process.env.AUDIO_SERVICE_URL;
+// const URL = 'http://localhost:3001';
+const URL = process.env.AUDIO_SERVICE_URL;
 
 export async function POST(req: Request) {
   const {videoId, token} = await req.json();
